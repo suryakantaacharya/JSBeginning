c3d8fc9b5517f468a68e1467cdaa3c1d374beeb3
suryakantaacharya <bubu.brl.sa@gmail.com>
2024-02-21 00:08:53 +0530

    day 1

---
+++ Day1/main.js
@@ -0,0 +1,15 @@
+console.log("Hello");
+
+//variables 
+let x = "x";
+x =  3;
+console.log(x);
+var v = "v"; //not used anymore
+const c = "c"; //only defined once can't be changed
+//c = 2 // fails throws error
+console.log(c)
+
+let string = "surya";
+let age = "23"
+let isMarried = false;
+console.log("Name of the person is" + string + "age is:" + age + "married" + isMarried);
